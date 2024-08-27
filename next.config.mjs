c101f052d612b626e219788546635cbf8d984854
Rishav Mondal <mondalrishav25@gmail.com>
2024-08-27 21:53:25 +0530

    👍

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  env: {
+    WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
+  },
 };
 
 export default nextConfig;
